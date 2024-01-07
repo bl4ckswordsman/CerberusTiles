02f2cac3a785d1a1a57557e816b5b126cf261fbf
amarildo <91825788+bl4ckswordsman@users.noreply.github.com>
2024-01-08 00:05:11 +0100

    fix(codacy): avoid codacy false positive

--- app/src/main/java/com/bl4ckswordsman/cerberustiles/MainActivity.kt
+++ app/src/main/java/com/bl4ckswordsman/cerberustiles/MainActivity.kt
@@ -20,8 +20,11 @@ import kotlinx.coroutines.launch
 /** Main activity of the app. */
 @RequiresApi(Build.VERSION_CODES.N_MR1)
 class MainActivity : ComponentActivity(), LifecycleObserver {
+
     /** TODO: Use viewModelScope for LiveData instead of using MutableLiveData, this ensures that
      * the LiveData is cleared when the ViewModel is cleared.**/
+
+
     private val shortcutManager by lazy { getSystemService(ShortcutManager::class.java) }
     private val shortcutHelper by lazy { ShortcutHelper(this, shortcutManager) }
 
