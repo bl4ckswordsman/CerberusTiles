1f2c0c7f87634cf8fc10bd1cc6a3f4ffbd32aae7
amarildo <amarildo.rajta@gmail.com>
2024-01-11 16:01:56 +0100

    feat: add quick setting tile for adaptive brightness

---
+++ app/src/main/java/com/bl4ckswordsman/cerberustiles/quicksettingstiles/AdaptiveBrightnessTileService.kt
@@ -0,0 +1,33 @@
+package com.bl4ckswordsman.cerberustiles.quicksettingstiles
+
+import android.graphics.drawable.Icon
+import android.service.quicksettings.Tile
+import android.service.quicksettings.TileService
+import com.bl4ckswordsman.cerberustiles.R
+import com.bl4ckswordsman.cerberustiles.SettingsUtils
+
+class AdaptiveBrightnessTileService : TileService() {
+
+    override fun onStartListening() {
+        super.onStartListening()
+
+        val isAdaptive = SettingsUtils.Brightness.isAdaptiveBrightnessEnabled(this)
+
+        qsTile.state = if (isAdaptive) Tile.STATE_ACTIVE else Tile.STATE_INACTIVE
+        qsTile.updateTile()
+    }
+
+    override fun onClick() {
+        super.onClick()
+
+        val isAdaptive = SettingsUtils.Brightness.isAdaptiveBrightnessEnabled(this)
+        SettingsUtils.Brightness.toggleAdaptiveBrightness(this)
+
+        qsTile.state = if (isAdaptive) Tile.STATE_INACTIVE else Tile.STATE_ACTIVE
+        val iconRes =
+            if (qsTile.state == Tile.STATE_ACTIVE) R.drawable.baseline_brightness_auto_24 else R.drawable.outline_brightness_auto_24
+        qsTile.icon = Icon.createWithResource(this, iconRes)
+        qsTile.updateTile()
+    }
+    // on hold, open the app
+}
