b3e9653147fcc3802fbd6bc4db69b582494c126a
amarildo <amarildo.rajta@gmail.com>
2024-01-14 18:46:29 +0100

    build: bump version to v0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.bl4ckswordsman.cerberustiles"
         minSdk = 24
         targetSdk = 34
-        versionCode = 4
-        versionName = "0.1.1"
+        versionCode = 5
+        versionName = "0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
