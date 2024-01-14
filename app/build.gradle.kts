3b7897d89120aa0337f90417a52d664f8960a271
amarildo <amarildo.rajta@gmail.com>
2024-01-14 20:01:04 +0100

    build: bump version to v0.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 5
-        versionName = "0.2.0"
+        versionName = "0.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
