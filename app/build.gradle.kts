0f0864e445d357a11c2ffd197d846266e14a3beb
amarildo <amarildo.rajta@gmail.com>
2024-01-16 20:51:25 +0100

    build: bump version to v0.2.5 for testing update feature

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.bl4ckswordsman.cerberustiles"
         minSdk = 24
         targetSdk = 34
-        versionCode = 8
-        versionName = "0.2.4"
+        versionCode = 9
+        versionName = "0.2.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
