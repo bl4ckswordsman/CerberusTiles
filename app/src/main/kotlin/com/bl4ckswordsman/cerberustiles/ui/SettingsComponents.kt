4fcc3bac5b4cbe30bdac0ea04aa15da30b6fa3e9
amarildo <amarildo.rajta@gmail.com>
2024-10-05 20:07:46 +0200

    chore: add documentation for ComponentVisibilityDialogParams data class

--- app/src/main/kotlin/com/bl4ckswordsman/cerberustiles/ui/SettingsComponents.kt
+++ app/src/main/kotlin/com/bl4ckswordsman/cerberustiles/ui/SettingsComponents.kt
@@ -4,6 +4,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.platform.LocalContext
 
+/**
+ * Parameters for controlling the visibility of settings components in the dialog.
+ *
+ * @property adaptBrightnessSwitch MutableState controlling the visibility of the adaptive brightness switch.
+ * @property brightnessSlider MutableState controlling the visibility of the brightness slider.
+ * @property vibrationSwitch MutableState controlling the visibility of the vibration switch.
+ */
 data class ComponentVisibilityDialogParams(
     val adaptBrightnessSwitch: MutableState<Boolean>,
     val brightnessSlider: MutableState<Boolean>,
