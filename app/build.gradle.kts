ef9abbec38e35474b01f09a37fac26bebfd768e7
amarildo <amarildo.rajta@gmail.com>
2024-12-24 15:20:25 +0100

    build: enable minification for release build and add applicationIdSuffix for debug build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,9 +24,12 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
             //signingConfig = signingConfigs.getByName("debug")
+        }
+        debug {
+            applicationIdSuffix = ".debug"
         }
     }
     compileOptions {
