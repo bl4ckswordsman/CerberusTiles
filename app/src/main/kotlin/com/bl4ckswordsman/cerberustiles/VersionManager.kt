04a09ba626f91e620a92a2d0519eb4b091b568ca
amarildo <amarildo.rajta@gmail.com>
2024-12-24 15:20:39 +0100

    fix: ensure version name is returned as a string in VersionManager

--- app/src/main/kotlin/com/bl4ckswordsman/cerberustiles/VersionManager.kt
+++ app/src/main/kotlin/com/bl4ckswordsman/cerberustiles/VersionManager.kt
@@ -33,7 +33,7 @@ class VersionManager {
             pInfo.versionName
         } catch (e: PackageManager.NameNotFoundException) {
             UNKNOWN
-        }
+        }.toString()
     }
 
     /**
